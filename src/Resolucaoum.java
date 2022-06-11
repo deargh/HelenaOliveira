0ef056c00b9921e3550085e160c5b6be81fd1ce3
Helena Oliveira <heloisalima_23@outlook.com>
2022-06-11 16:05:43 -0300

    Teste

---
+++ src/Resolucaoum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Resolucaoum {
+  public static void main(String[] args) {
+     // QUESTÃO 1 - OK
+     // int lado, area;
+     //
+     // Scanner leitor = new Scanner(System.in);
+     //  System.out.println("Digite o tamanho do lado:");
+     //    lado = leitor.nextInt();
+     //
+     //  area = lado*lado;
+     //   System.out.println(area); 
+
+     //QUESTÃO 2 -
+     int valor1, valor2, soma;
+
+     Scanner leitor = new Scanner(System.in);
+
+     System.out.println("Digite o primeiro valor:");
+     valor1 = leitor.nextInt();
+     System.out.println("Digite o segundo valor:");
+     valor2 = leitor.nextInt();
+
+     soma = valor1 + valor2;
+
+     System.out.println("A soma dos valores é igual a: " + soma);
+
+
+
+
+        
+
+        
+
+    }
+    
+}
